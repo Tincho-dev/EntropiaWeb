e9ebab5ebd8ccc12e6ba4f6bec8dee54473acde1
Martin Lopez Rubio <Martin.LopezRubio@alu.frt.utn.edu.ar>
2023-02-07 11:48:56 -0300

    CEDIAD MERGED

---
+++ Persistanse/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Model;
+using Models;
+using System.Data.Entity;
+
+namespace Persistanse
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Fuente> Fuentes { get; set; }
+        public DbSet<Letra> Letras { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ApplicationRol> ApplicationRole { get; set; }
+        public DbSet<ApplicationUserRole> ApplicationUserRoles { get; set; }
+
+
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ WebApp/Models/RolesViewModels.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Models
+{
+    public class RolesViewModels
+    {
+        public int Id_Rol { get; set; }
+        public string Nombre { get; set; }
+    }
+}

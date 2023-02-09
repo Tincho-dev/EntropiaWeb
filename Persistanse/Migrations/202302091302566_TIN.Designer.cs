66354419378fbfd0e85c5cdb275d9c1568143df4
Martin Lopez Rubio <Martin.LopezRubio@alu.frt.utn.edu.ar>
2023-02-09 11:13:21 -0300

    Actualizacion de los manuales

--- Persistanse/Migrations/202302091302566_TIN.Designer.cs
+++ Persistanse/Migrations/202302091302566_TIN.Designer.cs
@@ -7,13 +7,13 @@ namespace Persistence.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.0-30225")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class TIN : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(TIN));
         
         string IMigrationMetadata.Id
         {
-            get { return "202302081423049_initial"; }
+            get { return "202302091302566_TIN"; }
         }
         
         string IMigrationMetadata.Source
---
+++ Persistanse/Migrations/202302091302566_TIN.cs
@@ -0,0 +1,16 @@
+namespace Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TIN : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
